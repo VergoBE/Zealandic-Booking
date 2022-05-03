a7e373957f5c117b728dea03943c42f16780606b
RazorSDU <j_hoppe.r@hotmail.com>
2022-05-03 13:20:19 +0200

    Feck interfaces

--- Interfaces/IDBService.cs
+++ Interfaces/IDBService.cs
@@ -7,10 +7,5 @@ namespace Zealandic_Booking.Interfaces
 {
     public class IDBService<T>
     {
-        Task<IEnumerable<T>> GetObjectsAsync();
-        Task AddObjectAsync(T obj);
-        Task DeleteObjectAsync(T obj);
-        Task UpdateObjectAsync(T obj);
-        Task<T> GetObjectByIdAsync(int id);
     }
 }
