47ddd58c434a519ab640f7a68bdade6f96158ca5
Mads Christian Bechmann Sørensen <mads92n1@edu.easj.dk>
2022-05-04 11:11:42 +0200

    Delet done, missing HTML

---
+++ Pages/Booking/DeleteBooking.cshtml.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Zealandic_Booking.Services;
+
+namespace Zealandic_Booking.Pages.Booking
+{
+    [Authorize(Roles = "Teacher")]
+    public class DeleteBookingModel : PageModel
+    {
+        private BookingService _bookingService;
+        private List<Models.Booking> bookings;
+
+        public DeleteBookingModel(BookingService bookingService)
+        {
+            this._bookingService = bookingService;
+            bookings = bookingService.GetBookings().ToList();
+        }
+        [BindProperty] public int BookingID { get; set; }
+        public IActionResult OnGet()
+        {
+            return Page();
+        }
+        public async Task<IActionResult> OnPost()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+            _bookingService.DeleteBooking(BookingID);
+            return RedirectToPage("GetBookings");
+        }
+    }
+}
+
