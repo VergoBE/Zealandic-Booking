ae764cba414941158116be4bf75808b6249da27c
Johan Hoppe Rauer <joha681s@edu.zealand.dk>
2022-05-09 10:43:14 +0200

    Simple booking v1.1 + Room service change

--- Pages/WorkInProgress/SimpleBooking.cshtml.cs
+++ Pages/WorkInProgress/SimpleBooking.cshtml.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.IdentityModel.Tokens;
 using Zealandic_Booking.Services;
 
 namespace Zealandic_Booking.Pages.WorkInProgress
@@ -40,6 +41,21 @@ namespace Zealandic_Booking.Pages.WorkInProgress
             Users = userService.GetUsers().ToList();
             Rooms = roomService.GetRooms().ToList();
             return Page();
+        }
+
+        public int year { get; set; }
+        public int month { get; set; }
+        public int day { get; set; }
+        public Models.Room room { get; set; }
+        public Models.User user { get; set; }
+        public int time { get; set; }
+        public int postRoomID { get; set; }
+        public int postUserID { get; set; }
+
+        public void OnPost(int year, int month, int day, int postRoomID, int postUserID, int time)
+        {
+            room = roomService.GetRoom(postRoomID);
+            user = userService.GetUser(postUserID);
         }
     }
 }
