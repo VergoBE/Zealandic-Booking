e0184910d15bf33e186f1613ef47eda57578bef5
Morten Gram-Hansen <icyxgram@gmail.com>
2022-05-10 10:30:31 +0200

    Updated Getbookings to include user and rooms as objects so u can take Room.Name out and user.name out.

--- Pages/Booking/GetBookings.cshtml.cs
+++ Pages/Booking/GetBookings.cshtml.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Zealandic_Booking.Models;
 using Zealandic_Booking.Services;
 
 namespace Zealandic_Booking.Pages.Booking
@@ -15,10 +16,17 @@ namespace Zealandic_Booking.Pages.Booking
     public class GetBookingsModel : PageModel
     {
         private BookingService bookingService;
+        private UserService userService;
+        private RoomService roomService;
+        private IEnumerable<User> users;
+        private IEnumerable<Models.Room> rooms;
 
-        public GetBookingsModel(BookingService bookingService)
+        public GetBookingsModel(BookingService bookingService, UserService userService, RoomService roomService)
         {
             this.bookingService = bookingService;
+            this.userService = userService;
+            this.roomService = roomService;
+
         }
 
         public List<Models.Booking> Bookings { get; private set; }
@@ -27,6 +35,27 @@ namespace Zealandic_Booking.Pages.Booking
         public IActionResult OnGet()
         {
             Bookings = bookingService.GetBookings().ToList();
+            Bookings = (from booking in Bookings orderby booking.Time.Date select booking).ToList();
+            users = userService.GetUsers();
+            rooms = roomService.GetRooms();
+            foreach (var booking in Bookings)
+            {
+                foreach (var user in users)
+                {
+                    if (booking.UserID == user.UserID)
+                    {
+                        booking.User = user;
+                    }
+                }
+                foreach (var room in rooms)
+                {
+                    if (booking.RoomID == room.RoomID)
+                    {
+                        booking.Room = room;
+                    }
+                }
+            }
+
             return Page();
         }
     }
